798fcc73b678d3a83b3a792efeecf0d7960fa513
SabareeshKrishnan <krisshsabareesh@yahoo.co.in>
2016-10-08 10:42:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function(req, res) {
+    res.send('Hai There, request for atricle one is served here!!!');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
