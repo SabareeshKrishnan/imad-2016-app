8dfae8cc893a13e6d3cc98b8c8c603230b6a9ec9
SabareeshKrishnan <krisshsabareesh@yahoo.co.in>
2016-10-08 15:40:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var img = document.getElementById("madi");
 var marginleft = 0;
 function moveRight () {
     marginleft = marginleft + 10;
-    img.style.marginleft = marginleft + 'px';
+    img.style.marginLeft = marginleft + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveRight, 50); 
