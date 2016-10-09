2e3186fc70f9071a20a3345d00b13eb07229ec2a
SabareeshKrishnan <krisshsabareesh@yahoo.co.in>
2016-10-09 20:34:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,19 @@
 console.log('Loaded!');
+
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+  //make counter endpoint req
+  
+  //capture the response and store it in a variable
+  
+  //render variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
+
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 
