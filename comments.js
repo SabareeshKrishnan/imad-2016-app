47ed21b12f7b48a734b7feb53d572109fa7915be
SabareeshKrishnan <krisshsabareesh@yahoo.co.in>
2016-10-13 20:28:12 +0530

    [imad-console] Updates comments.js

--- comments.js
+++ comments.js
@@ -5,11 +5,32 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = (function() {
     
     //create request
+    var request = new XMLHttpRequest();
+
     var commentInput = document.getElementById('comment');
-    comment = commentInput.value;
+    comment = commentInput.value;    
+    
+    //capture the request and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //check status of the request next....
+            if (request.status === 200) {
+                var comments = request,responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for (var i=0; i<comments.length; i++) {
+                    list == names[i] + '<br>';
+                }
+                var commentarea =  document.getElementById('footer');
+                commentarea.innerHTML = list;
+            }
+        }
+    };
+
     
     
     
     //make request
     request.open('GET', 'http://sabareeshkrishnan.imad.hasura-app.io/submit-comment?comment=', comment, true);
+    request.send(null);
 });
