828809c551f0c28fed4d28880d53d48649a82531
SabareeshKrishnan <krisshsabareesh@yahoo.co.in>
2016-10-13 19:49:32 +0530

    [imad-console] Updates comments.js

--- comments.js
+++ comments.js
@@ -4,5 +4,5 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = (function() {
     var name = document.getElementById('comment')
-    comment.innerHTML = 'Value'
+    commentInput.value = 'Value'
 });
